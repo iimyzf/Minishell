acd9fc2206f93bb436beb652ef1e8e8e082c20c4
bahimzabir <azabir@student.1337.ma>
2022-06-27 22:23:48 +0100

    starting

--- srcs/execution/ft_env.c
+++ srcs/execution/ft_env.c
@@ -10,4 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../minishell.h"
 
+int	ft_env(char	**env)
+{
+	int	i;
+
+	i = -1;
+	while (env[++i])
+		printf("%s\n", env[i]);
+	return(1);
+}
--- srcs/execution/ft_pwd.c
+++ srcs/execution/ft_pwd.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../minishell.h"
 
+int	ft_pwd()
+{
+	char	*path;
+	char	*buf;
+
+	path = NULL;
+	while (path == NULL) {
+		path = getcwd(buf, 1);
+	}
+	printf("%s\n", path);
+	//system("leaks pwd");
+}
