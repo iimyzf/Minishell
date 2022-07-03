c5303481bfc835ead84e982c8a8cc54afe3f4290
bahimzabir <azabir@student.1337.ma>
2022-07-03 19:15:29 +0100

    handled 2 pipes

--- srcs/include/main.c
+++ srcs/include/main.c
@@ -24,37 +24,32 @@ void	free_array(char **arr)
 	}
 	free(arr);
 }
-/*
-void	lstprint(t_cmd	*cmd_list)
-{
-	t_cmd 	*temp;
-	char	*path;
-	char	*tmp;
-	int		id;
 
-	temp = cmd_list;
-	while (temp && (temp->id != 1))
+void	process(char **cmd, char *path, t_data *data)
+{
+	pid_t	pid;
+	//int		fd[2];
+
+	pid = fork();
+	if (pid == 0)
 	{
-		tmp = ft_strjoin2(tmp, temp->cmd);
-		temp = temp->next;
+		close(data->fd[0]);
+		dup2(data->fd[1], STDOUT_FILENO); //dup2 copies the content of its arg 2 to its arg 1!
+		//dup2(fd[1], STDOUT_FILENO); //dup2 copies the content of its arg 2 to its arg 1!
+		execve(path, cmd, NULL);
+		exit(1);
 	}
-	temp = cmd_list;
-	cmd_list->full_cmd = ft_split(tmp, ' ');
-	free (tmp);
-	if (path = check_path(cmd_list->full_cmd[0]), !path)
+	waitpid(pid, NULL, 0);
+	/*else
 	{
-		printf("minishell: %s: command not found\n", cmd_list->full_cmd[0]);
-		return;
-	}
-	id = fork();
-	if (id == 0)
-		execve(path, cmd_list->full_cmd, NULL);
-	else
-		waitpid(id, NULL, 0);
-	free(path);
-	free_array(cmd_list->full_cmd);
+		dup2(data->fd[0], STDIN_FILENO);
+		// write (1, "here1\n", 6);
+		close(data->fd[1]);
+		waitpid(pid, NULL, 0);
+		// write (1, "here2\n", 6);
+	}*/
 }
-*/
+
 void	ft_parce(t_data *data)
 {
 	t_lexer	*lexer;
@@ -62,9 +57,11 @@ void	ft_parce(t_data *data)
 	int		i;
 	t_cmd	*temp;
 	char	*tmp;
+	char	*path;
 
 	i = 0;
 	tmp = "";
+	pipe(data->fd);
 	lexer = lexer_init(data->input);
 	(data)->cmd_list = NULL;
 	while ((token = lexer_get_next_token(lexer)) != NULL)
@@ -79,43 +76,58 @@ void	ft_parce(t_data *data)
 	temp = (data)->cmd_list;
 	while (temp && (temp->id != -1))
 	{
-		while (temp && (temp->id != 8))
+		while (temp->id != -1 && (temp->id != 8))
 		{
 			tmp = ft_strjoin2(tmp, temp->cmd);
 			temp = temp->next;
 		}
-		if (temp)
+		if (temp && temp->id != -1)
 			temp = temp->next;
 		data->full_cmd = ft_split(tmp, ' ');
-		free (tmp);
-		i = 0;
-		while (data->full_cmd[i])
-		{
-			printf("[%s] ", data->full_cmd[i]);
-			i++;
-		}
+		tmp = "";
+		path = check_path(data->full_cmd[0]);
+		if (temp && temp->id == -1)
+			break;
+		process(data->full_cmd, path, data);
+		//free(path);
 		free_array(data->full_cmd);
-		printf("\n#######################\n");
 	}
+	close(data->fd[1]);
+	dup2(data->fd[0], STDIN_FILENO);
+	dup2(1, STDOUT_FILENO);
+	//path = check_path(data->full_cmd[0]);
+	execve(path, data->full_cmd, NULL);
+	waitpid(i, NULL, 0);
+	close(data->fd[0]);
+	free_array(data->full_cmd);
+	free (path);
 }
 
 
 int main(int ac, char **av, char **env)
 {
 	t_data	data;
+	int		pid;
 
 	if (ac != 1)
 		return (1);
 	av[0] = "minishell> ";
 	data.env = env;
 	free (data.full_cmd);
+	data.in = 1;
 	while (1)
 	{
 		data.input = readline(av[0]);
-		add_history(data.input);
-		if (!ft_strcmp(data.input, "exit"))
-			break;
-		ft_parce(&data);
+		if (data.input[0])
+		{
+			if (!ft_strcmp(data.input, "exit"))
+				break;
+			pid = fork();
+			add_history(data.input);
+			if (pid == 0)
+				ft_parce(&data);
+		}
+		waitpid(pid, NULL, 0);
 	}
 	//system("leaks minishell");
 	return (0);
--- srcs/libft/ft_strcmp.c
+++ srcs/libft/ft_strcmp.c
@@ -15,7 +15,6 @@
 int	ft_strcmp(char *str1, char *str2)
 {
 	int	i;
-
 	i = 0;
 	while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0')
 		i++;
