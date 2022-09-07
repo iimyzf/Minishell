6ac4c4b33b4bc95e511752ad3ec9b35c412891ad
bahimzabir <azabir@student.1337.ma>
2022-09-07 12:54:36 +0100

    check path done

--- srcs/execution/check_path.c
+++ srcs/execution/check_path.c
@@ -38,24 +38,12 @@ int is_dir(char *cmd)
 
 }
 
-char	*path_checker(t_data *data, char *cmd, char **env)
+char	*creat_path(t_data *data, char *cmd)
 {
+	int	i;
 	char	*cmd_path;
 	char	*part_of_path;
-	int		i;
-	//struct stat buf;
 
-	i = 0;
-	if (!cmd || !*cmd)
-		return (NULL);
-	//fprintf(stderr, "access = %d\n",access(cmd, X_OK));
-	if (!access(cmd, F_OK) && !access(cmd, X_OK))
-		return(ft_strjoin(cmd, ""));
-	while (env[i] && ft_strnstr(env[i], "PATH", 4) == 0)
-		i++;
-	if (env[i] == NULL)
-		return (NULL);
-	data->complete_path = ft_split(env[i] + 5, ':');
 	i = 0;
 	while (data->complete_path[i])
 	{
@@ -72,4 +60,25 @@ char	*path_checker(t_data *data, char *cmd, char **env)
 	}
 	free_array(data->complete_path);
 	return (NULL);
+}
+
+char	*path_checker(t_data *data, char *cmd, char **env)
+{
+	int		i;
+	struct stat buf;
+
+	i = 0;
+	if (!cmd || !*cmd)
+		return (NULL);
+	stat(cmd, &buf);
+	if (S_ISDIR(buf.st_mode))
+		return(NULL);
+	if (!access(cmd, F_OK) && !access(cmd, X_OK))
+		return(ft_strjoin(cmd, ""));
+	while (env[i] && ft_strnstr(env[i], "PATH", 4) == 0)
+		i++;
+	if (env[i] == NULL)
+		return (NULL);
+	data->complete_path = ft_split(env[i] + 5, ':');
+	return (creat_path(data, cmd));
 }
